431a78e3aeefe1229da1c2d06f9350e441f5a4f8
Ubuntu <ubuntu@ip-172-31-10-109.us-west-1.compute.internal>
2025-08-03 15:53:20 +0000

    name changes

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "dev-proj-1-jenkins-remote-state-bucket-nikhil-123456"
-    key    = "Terraform-setup/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-west-1"
   }
 }
